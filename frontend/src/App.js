271b908d349441b2ad9e2e0bc10ee30246b484c1
Ryan C <rcarlton@protonmail.com>
2021-12-16 19:51:23 -0500

    Adding two api calls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,38 @@ import logo from './logo.svg'
 import './App.scss'
 import AccPage from './components/Acc'
 import TimelinePage from './components/Timeline'
-import Amplify, {Auth} from 'aws-amplify'
+import Amplify, {Auth, API} from 'aws-amplify'
 import awsExports from './aws-exports'
-import {withAuthenticator} from '@aws-amplify/ui-react';
+import {withAuthenticator} from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
-Amplify.configure(awsExports);
+Amplify.configure(awsExports)
 
 
 function App() {
-
+    let testRootRoute = async() => {
+        let params = {
+            headers: {
+                'Authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
+            }
+        }
+        let otherTest = await API.get('uiAPI', '/uiapi', params)
+        console.log(otherTest)
+    }
+    let testSubroute = async() => {
+        let params = {
+            headers: {
+                'Authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
+            }
+        }
+        let otherTest = await API.get('uiAPI', '/uiapi/', {})
+        console.log(otherTest)
+    }
     return (
         <div className="App">
             <TimelinePage/>
+            <button onClick={testRootRoute}>Test Root Call</button>
+            <button onClick={testSubroute}>Test Root Call</button>
             {/*<AccPage/> */}
         </div>
     
